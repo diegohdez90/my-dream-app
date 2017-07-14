c271fe857656e94d8ed0d6dd66b4ada83ffa5ac4
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-07-14 17:36:24 -0500

    Fix RouterModule from forChild to forRoot

--- src/app/app-router/app-router-routing.module.ts
+++ src/app/app-router/app-router-routing.module.ts
@@ -3,14 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { GizmoComponent } from '../gizmo/gizmo.component'
 
+
 const routes: Routes = [
-	{ path : '', component : GizmoComponent },
 	{ path : 'gizmo', component : GizmoComponent },
 	{ path : '**', redirectTo : '/' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: []
 })
